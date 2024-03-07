9d32351ac273ee1af28749b2d41019cee406ac55
Elliott Sánchez León <raptordroid15@gmail.com>
2024-03-07 12:06:05 -0600

    Manejo y optimización de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,19 @@
+import styles from './Description.module.sass'
+import Image from 'next/image'
+
 export const Description = () => {
     return (
-        <section>
-            <img src="/images/description.jpeg" alt="products marketplace" />
-            <h2>Description</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic magnam, quod perferendis cum officia alias expedita explicabo ad labore earum, necessitatibus corrupti neque? Ea, qui officiis repellendus dignissimos ad odit!</p>
+        <section className={styles.Description}>
+            <Image
+                src="/images/description.jpeg"
+                alt="products marketplace"
+                width={500}
+                height={300}
+            />
+            <div className={styles.Description__text}>
+                <h2>Description</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic magnam, quod perferendis cum officia alias expedita explicabo ad labore earum, necessitatibus corrupti neque? Ea, qui officiis repellendus dignissimos ad odit!</p>
+            </div >
         </section>
     )
 }
