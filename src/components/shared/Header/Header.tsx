bef299e2f053c5e644c779e6888f7b59214da88c
Elliott Sánchez León <raptordroid15@gmail.com>
2024-03-07 22:53:24 -0600

    Creando estilos dinámicos con condicionales

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export const Header = () => {
     return (
-        <header>
+        <header style={{ textAlign: 'center' }}>
             <nav>
-                <ul>
+                <ul style={{ display: 'inline-block', padding: 0 }}>
                     <Link href="/">
-                        <li>Home</li>
+                        <li style={{ display: 'inline-block', margin: '0 10px' }}>Home</li>
                     </Link>
                     <Link href="/store">
-                        <li>Store</li>
+                        <li style={{ display: 'inline-block', margin: '0 10px' }}>Store</li>
                     </Link>
                 </ul>
             </nav>
