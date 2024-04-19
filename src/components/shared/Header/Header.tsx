31adec349b321f4c2ca5b32bc414af57461f4843
Elliott Sánchez León <raptordroid15@gmail.com>
2024-04-19 10:48:45 -0600

    Como implementar un flujo de login en un proyecto

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link';
+import {cookies} from 'next/headers'
 
 export const Header = () => {
+
+    const cookiesStore = cookies()
+    const token = cookiesStore.get('accesToken')?.value
+
     return (
         <header style={{ textAlign: 'center' }}>
             <nav>
@@ -12,6 +17,8 @@ export const Header = () => {
                         <li style={{ display: 'inline-block', margin: '0 10px' }}>Store</li>
                     </Link>
                 </ul>
+
+                {token ? (<p>Hola</p>) : (<Link href="/login">Login</Link>)}
             </nav>
         </header>
     )
--- src/utils/auth/createAccessToken.ts
+++ src/utils/auth/createAccessToken.ts
@@ -1,24 +1,36 @@
 import { GraphQLClientSingleton } from "app/graphql"
 import { customerAccessTokenCreateMutation } from "app/graphql/mutations/customerAccesTokenCreate"
-import { cookies } from "next/headers"
+import { cookies } from 'next/headers'
 
 export const createAccessToken = async (email: string, password: string) => {
-    const cookiesStore = cookies()
-    const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
-    const {customerAccessTokenCreate} = await graphqlClient.request(customerAccessTokenCreateMutation, {
-        "email": email,
-        "password": password
+  const cookiesStore = cookies()
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+  const { customerAccessTokenCreate }: {
+    customerAccessTokenCreate: {
+      customerAccessToken: {
+        accessToken: string
+        expiresAt: string
+      }
+    }
+  }
+    = await graphqlClient.request(customerAccessTokenCreateMutation, {
+      "email": email,
+      "password": password
     })
 
-    const {accesToken, expiresAt} = customerAccessTokenCreate?.customerAccessToken
+    if (customerAccessTokenCreate && customerAccessTokenCreate.customerAccessToken) {
+        const { accessToken, expiresAt } = customerAccessTokenCreate.customerAccessToken;
+        
 
-    if(accesToken){
-        cookiesStore.set("accessToken", accesToken, {
-            path: "/",
-            expires: new Date(customerAccessTokenCreate.expireAt),
-            httpOnly: true,
-            sameSite: "strict",
-        })
-    }
+  if (accessToken) {
+    cookiesStore.set("accessToken", accessToken, {
+      path: "/",
+      expires: new Date(expiresAt),
+      httpOnly: true,
+      sameSite: "strict"
+    })
 
+    return accessToken
+  }
+}
 }
