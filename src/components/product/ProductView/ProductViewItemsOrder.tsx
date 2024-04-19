c6ee31651c06edc4d9bd5da63409e399bb3a31a4
Elliott Sánchez León <raptordroid15@gmail.com>
2024-04-19 12:41:18 -0600

    Manejo de estado con zustand

--- src/components/product/ProductView/ProductViewItemsOrder.tsx
+++ src/components/product/ProductView/ProductViewItemsOrder.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { SyntheticEvent, useState } from "react";
 import styles from "./ProductViewItemsOrder.module.sass";
+import { useShoppingCart } from "app/hooks/useShoppingCart";
 
 
 interface ProductViewItemsOrderProps {
@@ -10,6 +11,7 @@ interface ProductViewItemsOrderProps {
 
 export const ProductViewItemsOrder = ({ maxQuantity, product }: ProductViewItemsOrderProps) => {
     const [counter, setCounter] = useState(1);
+    const { addToCart } = useShoppingCart();
 
     const handleAddToCart = (event: SyntheticEvent) => {
         event.preventDefault();
--- types.d.ts
+++ types.d.ts
@@ -3,3 +3,24 @@ interface ErrorPageProps {
     reset: () => void;
 
 }
+
+type ProductType = {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    image: string;
+    quantity: number;
+    handle: string;
+    tags: string;
+    gql_id: string;
+  };
+  
+  type CartItem = {
+    title: string;
+    price: number;
+    quantity: number;
+    id: string;
+    image: string;
+    merchandiseId: string;
+  }
